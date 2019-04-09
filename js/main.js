ee29c3c06d08fe2c1327dcf1f9ecf86461f50338
Anam <anammainfolder@Anams-MacBook-Pro.local>
2019-04-09 19:31:51 +1200

    addition of footer

--- js/main.js
+++ js/main.js
@@ -2,6 +2,6 @@ window.onscroll = () => {
     if(this.scrollY <= 5) {
       $('.navbar').css('background-color', 'transparent');
     } else {
-      $('.navbar').css('background-color', 'white');
+      $('.navbar').css('background-color', '#01acec');
     }
   };
