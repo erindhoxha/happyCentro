16a87142e2cfbe976f97c7e44ea8f565bba75dfe
Erind Hoxha <932400893@cit3-501m-22.local>
2019-04-10 10:00:38 +1200

    added some css transitions

--- js/main.js
+++ js/main.js
@@ -1,7 +1,15 @@
 window.onscroll = () => {
     if(this.scrollY <= 5) {
       $('.navbar').css('background-color', 'transparent');
+      $(".nav-link").css('color', 'white');
+      $(".find-us-button").css('color', 'white');
+      $(".find-us-button").css('border','2px solid white');
+      $(".navbar-brand img").css('filter', 'invert(0)');
     } else {
-      $('.navbar').css('background-color', '#01acec');
+      $('.navbar').css('background-color', '#fff');
+      $(".nav-link").css('color', 'black');
+      $(".find-us-button").css('color', 'black');
+      $(".find-us-button").css('border','2px solid black');
+      $(".navbar-brand img").css('filter', 'invert(1)');
     }
   };
