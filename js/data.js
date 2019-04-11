da518d95863567deaddf9c980e2bd16fd739b97b
Erind Hoxha <932400893@cit3-501m-22.local>
2019-04-12 11:08:25 +1200

    added handlebars

--- js/data.js
+++ js/data.js
@@ -1,27 +1,141 @@
   // adding object for designers
 
-  designerDetails = 
-    [ {
-      name : 'Anam',
-      id : '`1234545',
-      impressions: 'xxx',
-      img : 'images/anamPic.jpg',
-    },
-    {
-      name : 'Erind',
-      id : '`1234545',
-      impressions: 'xxx',
-      img : 'images/erindPic.jpg',
-    },
-    {
-      name : 'Farisha',
-      id : '`1234545',
-      impressions: 'xxx',
-      img : 'images/farishaPic.jpg',
-    },
+  designerDetails = {
+    people: [
+      {
+        name : 'Anam',
+        id : '`1234545',
+        impressions: 'xxx',
+        img : 'img/person.jpeg',
+        likes:14,
+        behanceID: '123124125172561257'
+      },
+      {
+        name : 'Erind',
+        id : '`1234545',
+        impressions: 'xxx',
+        img : 'img/person1.jpeg',
+        likes:14,
+        behanceID: '123124125172561257'
+      },
+      {
+        name : 'Farisha',
+        id : '`1234545',
+        impressions: 'xxx',
+        img : 'img/person.jpeg',
+        likes:14
+      },
+      {
+        name : 'Anam',
+        id : '`1234545',
+        impressions: 'xxx',
+        img : 'img/person.jpeg',
+        likes:14
+      },
+      {
+        name : 'Erind',
+        id : '`1234545',
+        impressions: 'xxx',
+        img : 'img/person1.jpeg',
+        likes:14
+      },
+      {
+        name : 'Farisha',
+        id : '`1234545',
+        impressions: 'xxx',
+        img : 'img/person.jpeg',
+        likes:14
+      },
+      {
+        name : 'Anam',
+        id : '`1234545',
+        impressions: 'xxx',
+        img : 'img/person.jpeg',
+        likes:14
+      },
+      {
+        name : 'Erind',
+        id : '`1234545',
+        impressions: 'xxx',
+        img : 'img/person1.jpeg',
+        likes:14
+      },
+      {
+        name : 'Farisha',
+        id : '`1234545',
+        impressions: 'xxx',
+        img : 'img/person.jpeg',
+        likes:14
+      },
+    ],
+  }
 
 
-    ];
+  // PROJECT DETAILS
+
+  projectDetails = {
+    project: [
+      {
+        name : 'Project HappyCentro',
+        id : '`1234545',
+        impressions: 'xxx',
+        img : 'img/project5.jpeg',
+        likes:14,
+        behanceID: '123124125172561257'
+      },
+      {
+        name : 'Erind',
+        id : '`1234545',
+        impressions: 'xxx',
+        img: 'img/project2.jpeg',
+        likes:14,
+        behanceID: '123124125172561257'
+      },
+      {
+        name : 'Farisha',
+        id : '`1234545',
+        impressions: 'xxx',
+        img: 'img/project2.jpeg',
+        likes:14
+      },
+      {
+        name : 'Anam',
+        id : '`1234545',
+        impressions: 'xxx',
+        img : 'img/project5.jpeg',
+        likes:14
+      },
+      {
+        name : 'Erind',
+        id : '`1234545',
+        impressions: 'xxx',
+        img : 'img/project5.jpeg',
+        likes:14
+      },
+      {
+        name : 'Farisha',
+        id : '`1234545',
+        impressions: 'xxx',
+        img : 'img/project2.jpeg',
+        likes:14
+      },
+      {
+        name : 'Anam',
+        id : '`1234545',
+        impressions: 'xxx',
+        img : 'img/person.jpeg',
+        likes:14
+      },
+      {
+        name : 'Erind',
+        id : '`1234545',
+        impressions: 'xxx',
+        img : 'img/person1.jpeg',
+        likes:14
+      },
+    ],
+  }
+
 
     // testing back to top scroll (check to see if erind has easier code)
 
